7fb658c9ff5c2d8fd54e2c84fd66ef970e270eac
ste2021 <estela.oliveira.cont@gmail.com>
2022-12-03 17:44:53 -0300

    operações com números

---
+++ aulas/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Estelinha'
+sobrenome = 'Jones'
+idade = 20
+ano_nascimento = 2022 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.65
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
